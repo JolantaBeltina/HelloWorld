2f390b39e8f571cee4aa67ab4e1811d094d2f827
JolantaBeltina <112900391+JolantaBeltina@users.noreply.github.com>
2022-09-29 20:33:07 +0300

    Mājas Darbs Java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mani sauc Jolanta");
-        System.out.println("Es esmu studente");
+
     }
 }
--- src/main/java/lekcijaViens/majasDarbs/Main/Java.java
+++ src/main/java/lekcijaViens/majasDarbs/Main/Java.java
@@ -1,7 +1,6 @@
-
 package lekcijaViens.majasDarbs.Main;
 
-public class main {
+public class Java {
 
     public static void main(String[] args) {
 
@@ -16,18 +15,18 @@ public class main {
         char valuta = '$';
         float AtlantijasOkeans = 329.7F;
 
-        System.out.println( ValstsNosaukums +   " valsts platība ir:"+ ValstsPlatiba + "km2;");
-        System.out.println(ValstsNosaukums + " galvaspilsētas nosaukums ir "+ Galvaspilseta  + ";");
-        System.out.println(ValstsNosaukums + " valsts valodas ir : "+ ValstsValoda1 + " un " + ValstsValoda2 + ";");
-        System.out.println("Vai " + ValstsNosaukums +  " ir EU? - " + navEU +";");
-        System.out.println("Vai " + ValstsNosaukums +  " ir ASV? - " + irUSA + ";");
+        System.out.println( ValstsNosaukums +   " valsts platība ir:"+ ValstsPlatiba + "km2");
+        System.out.println(ValstsNosaukums + " galvaspilsētas nosaukums ir "+ Galvaspilseta );
+        System.out.println(ValstsNosaukums + " valsts valodas ir : "+ ValstsValoda1 + " un " + ValstsValoda2 );
+        System.out.println("Vai " + ValstsNosaukums +  " ir EU? - " + navEU);
+        System.out.println("Vai " + ValstsNosaukums +  " ir ASV? - " + irUSA);
         System.out.println( ValstsNosaukums +   " valsts valūta ir: "+ valuta + " ASV dolārs.");
         System.out.println( ValstsNosaukums +   " ziemeļos apskalo Atlantijas okeāns "+ AtlantijasOkeans + " miljoni km3. ");
 
         System.out.println( ValstsNosaukums + " oficiāli Puertoriko Asociētā Valsts ir ASV valdījums Vestindijā."
                 + ValstsPlatiba +
                 " km2 platība - Aizņem Puertoriko salu (8674 km²), kā arī Vjekesu (134 km²), Monu (54 km²), Kulebru (28 km²) un citas mazākas salas. "
-                + Galvaspilseta + " pilsētu dibināja spāņu kolonizatori 1521. gadā. Puertoriko galvaspilsēta ir otra vecākā eiropiešu " +
+         + Galvaspilseta + " pilsētu dibināja spāņu kolonizatori 1521. gadā. Puertoriko galvaspilsēta ir otra vecākā eiropiešu " +
                 "dibinātā pilsēta Amerikā aiz Dominikānas galvaspilsētas Santodomingo."
                 + ValstsValoda1 + " un " + ValstsValoda2 +
                 " ir bieži sastopama vietējo sarunvalodās. "
