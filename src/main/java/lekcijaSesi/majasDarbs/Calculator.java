b35c8d33b209823fd27120c8cda26c0da2bc9ed6
JolantaBeltina <112900391+JolantaBeltina@users.noreply.github.com>
2022-11-04 16:08:59 +0200

    Calculator majas darbs

--- src/main/java/lekcijaSesi/majasDarbs/Calculator.java
+++ src/main/java/lekcijaSesi/majasDarbs/Calculator.java
@@ -12,6 +12,10 @@ public class Calculator {
     public static int add(int a, int b, int c){
         return a+b+c;
     }
+
+    public static int minus (int a, int b){return a-b;}
+    public static int divided (int a, int b){return a/b;}
+    public static int multiply (int a, int b){return a*b;}
 }
 
 
--- src/test/java/lekcijaSesiTests/majasDarbs/TestCalculator.java
+++ src/test/java/lekcijaSesiTests/majasDarbs/TestCalculator.java
@@ -22,12 +22,36 @@ public class TestCalculator {
             Assert.fail();}
         }
         @Test
-        public void testABCsumm (){
-        int resultABC = calculator.add(22,68,96);
-        if (resultABC !=186){
-            Assert.fail();
+        public void testABCsumm () {
+            int resultABC = calculator.add(22, 68, 96);
+            if (resultABC != 186) {
+                Assert.fail();
+            }
         }
+        @Test
+        public void testMinus(){
+
+        Assert.assertEquals(3,calculator.minus(9,6));}
+
+        @Test
+        public void testDivided(){
+        Assert.assertEquals(2,calculator.divided(10,5));}
+
+        @Test
+    public void testMultiply(){
+        Assert.assertEquals(100,calculator.multiply(10,10));
+        }
+
+    
+
+
+
+
+
+
+
+
     }
 
 
-}
+
